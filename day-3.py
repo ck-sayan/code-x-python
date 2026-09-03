851dfb35ab32cef0bf01dc3920300428e87b6d82
Sayan Chakraborty <105535261+ck-sayan@users.noreply.github.com>
2026-09-03 17:04:49 +0530

    added new project

--- day-3.py
+++ day-3.py
@@ -6,7 +6,7 @@ initial_age = float(input("well, how old are you buddy? You seem new to the dojo
 
 age = int(initial_age + 10)
 
-print(f"welcome to the dojo, {name}! At {initial_age}, I was already a street fighting shark, you are late so let's start the training!")
+print(f"welcome to the dojo, {name}! At {initial_age} years old, I was already a street fighting shark, you are late so let's start the training!")
 
 print(f"for getting the black belt, you'll need to train until {age}")
 
@@ -19,15 +19,15 @@ width = float(input("Enter the width of the rectangle = "))
 per = 2*(length + width)
 area = length*width
 
-print(f"For a rectangle with length = {length} and breadth = {width}, the perimeter should be {per} and area should be {area}")
+print(f"For a rectangle with length = {length}cm and breadth = {width}cm, the perimeter should be {per}cm and area should be {area}cm²")
 
 
 # Practice Problem:
 
 ## Calculate the area and perimeter of a square
 
-side_square = float(input("Enter the length of side of the square = "))
+side_square = float(input("Enter the length of side of the square (in cm) = "))
 per_square = 4*side_square
 area_square = side_square*side_square
 
-print(f"for a square of a side {side_square} and perimeter is {per_square} and area is {area_square}")
+print(f"for a square of a side {side_square}cm and perimeter is {per_square}cm and area is {area_square}cm²")
---
+++ madlibs_game.py
@@ -0,0 +1,17 @@
+# Madlibs game is a fun way to practice string formatting in Python. In this game, the user will be prompted to enter various words (nouns, verbs, adjectives, etc.) and those words will be inserted into a pre-defined story template to create a funny or interesting story.
+
+print("===============================================================\n💀 THE WORST DAY EVER 💀\n===============================================================")
+
+# The Character Information
+print("The Character\n")
+print("Name")
+name=input("Give me the name of our unfortunate protagonist: ")
+print("Occupation")
+occupation=input("What is their completely respectable occupation?: ")
+print("Country/City")
+c=input("Where does this disaster begin?: ")
+
+
+# Random objects
+print("\nNoun")
+noun=input("Give me a completely random object: ")
